4f62e5a520dd757c010723db8aac987557a550e3
Zachary Foster <zacharyfoster1989@gmail.com>
2014-07-11 11:35:05 -0700

    gzip compression

--- pack_installer.sh
+++ pack_installer.sh
@@ -4,5 +4,5 @@ mkdir alignreads_$1
 tar -cf	uninstalled_content.tar	uninstalled_content
 cp install.py uninstalled_content.tar alignreads_$1
 rm -rf uninstalled_content.tar uninstalled_content
-tar -cf alignreads_$1.tar alignreads_$1
+tar -zcf alignreads_$1.tar.gz alignreads_$1
 rm -rf alignreads_$1
