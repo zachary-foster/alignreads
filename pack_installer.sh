33b69e001116e541c25ae25b6db233a6bc9936db
Zachary Foster <zacharyfoster1989@gmail.com>
2014-07-11 09:59:27 -0700

    new; made installer tar file

---
+++ pack_installer.sh
@@ -0,0 +1,7 @@
+mkdir uninstalled_content
+cp alignreads.py default_configuration.py makeconsensus.py README.txt LICENSE readtools_exceptions.py readtools.py runyasra.py uninstalled_content
+mkdir alignreads_$1
+tar -cf	uninstalled_content.tar	uninstalled_content
+cp install.py uninstalled_content.tar alignreads_$1
+rm -rf uninstalled_content.tar uninstalled_content
+tar -cf alignreads_$1
