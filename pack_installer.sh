a0b43dcef5cb09d940f5ae7d2bacab29ab468c74
fosterz <zacharyfoster1989@gmail.com>
2014-07-17 11:43:07 -0700

    added README and INSTALL to top level dir

--- pack_installer.sh
+++ pack_installer.sh
@@ -2,7 +2,7 @@ mkdir uninstalled_content
 cp alignreads.py default_configuration.py makeconsensus.py README.txt LICENSE readtools_exceptions.py readtools.py runyasra.py uninstalled_content
 mkdir alignreads_$1
 tar -cf	uninstalled_content.tar	uninstalled_content
-cp install.py uninstalled_content.tar alignreads_$1
+cp install.py README.txt INSTALL.txt uninstalled_content.tar alignreads_$1
 rm -rf uninstalled_content.tar uninstalled_content
 tar -zcf alignreads_$1.tar.gz alignreads_$1
 rm -rf alignreads_$1
